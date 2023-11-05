348426dcfc1d90539da3fbfcb2650997c6cd9e89
Oscar Järgren <31920401+oscarjaergren@users.noreply.github.com>
2023-11-05 01:16:39 +0000

    Created Tests for the search scenarios per acceptance critera

--- Jargar.MorningStar.Search.Api.Tests/PersonControllerTests.cs
+++ Jargar.MorningStar.Search.Api.Tests/PersonControllerTests.cs
@@ -2,10 +2,8 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System.Net;
-using System.Net.Http.Json;
-using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json;
 
 namespace Jargar.MorningStar.Search.Api.Tests;
 
@@ -34,6 +32,29 @@ public static class PersonControllerTests
         {
             var response =  await _fixture.CreateClient().GetAsync("/Person");
             response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Theory]
+        [InlineData("James", new[] { "James Kubu", "James Pfieffer" })]
+        [InlineData("jam", new[] { "James Kubu", "James Pfieffer", "Chalmers Longfut" })]
+        [InlineData("Katey Soltan", new[] { "Katey Soltan" })]
+        [InlineData("Jasmine Duncan", new string[0])]
+        [InlineData("", new string[0])]
+        public async Task Search_Person_Returns_Expected_Results(string searchTerm, string[] expectedResults)
+        {
+            // Arrange
+            using var client = _fixture.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/person/search?term={Uri.EscapeDataString(searchTerm)}");
+
+            // Act
+            using var response = await client.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var actualResults = JsonSerializer.Deserialize<string[]>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            actualResults.Should().BeEquivalentTo(expectedResults);
         }
     }
 }
